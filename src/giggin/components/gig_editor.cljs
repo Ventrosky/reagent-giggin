36b2352cff8fd8b29c4cd8509424e231a7d6f6ae
Salvatore Ventrone <ventrosky@gmail.com>
2019-06-30 15:43:41 +0200

    Refactor Gigs

--- src/giggin/components/gig_editor.cljs
+++ src/giggin/components/gig_editor.cljs
@@ -10,39 +10,40 @@
                         :on-change #(swap! values assoc (keyword id) (.. % -target -value))}]])
 
 (defn gig-editor
-  [modal values insert-gig]
-  [:div.modal (when @modal {:class "active"})
+  [{:keys [modal values upsert-gig toggle-modal initial-values]}]
+  (let [{:keys [title desc img price sold-out]} @values]
+  [:div.modal (when (:active @modal) {:class "active"})
    [:div.modal__overlay]
    [:div.modal__container
     [:div.modal__body
      [form-group {:id "title"
                   :type "text"
-                  :value (:title @values)
+                  :value title
                   :values values}]
      [form-group {:id "desc"
                   :type "text"
-                  :value (:desc @values)
+                  :value desc
                   :values values}]
      [form-group {:id "img"
                   :type "text"
-                  :value (:img @values)
+                  :value img
                   :values values}]
      [form-group {:id "price"
                   :type "number"
-                  :value (:price @values)
+                  :value price
                   :values values}]
      [:div.form__group
       [:label.form__label {:for "sold-out"} "sold-out"]
       [:label.form__switch
        [:input {:type :checkbox
-                :checked (:sold-out @values)
+                :checked sold-out
                 :on-change #(swap! values assoc :sold-out (.. % -target -checked))}]
        [:i.form__icon]]]]
     
     [:div.modal__footer
      [:button.btn.btn--link.float--left
-      {:on-click #(reset! modal false)}
+      {:on-click #(toggle-modal {:active false :gig initial-values})}
       "Cancel"]
      [:button.btn.btn--secondary
-      {:on-click #(insert-gig @values)}
-      "Save"]]]])
+      {:on-click #(upsert-gig @values)}
+      "Save"]]]]))
--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -7,30 +7,42 @@
 
 (defn gigs
   []
-  (let [modal (r/atom false)
-        values (r/atom {:id (str "gig-" (random-uuid)) :title "" :desc "" :price 0 :img "" :sold-out false})
+  (let [modal (r/atom {:active false})
+        initial-values {:id nil :title "" :desc "" :price 0 :img "" :sold-out false}
+        values (r/atom initial-values)
         add-to-order #(swap! state/orders update % inc)
-        insert-gig (fn [{:keys [id title desc price img sold-out]}]
-                     (swap! state/gigs assoc id {:id id
+        toggle-modal (fn
+                       [{:keys [active gig]}]
+                       (swap! modal assoc :active active)
+                       (reset! values gig ))
+        upsert-gig (fn [{:keys [id title desc price img sold-out]}]
+                     (swap! state/gigs assoc id {:id (or id (str "gig-" (random-uuid)))
                                                  :title (str/trim title)
                                                  :desc (str/trim desc)
                                                  :img (str/trim img)
                                                  :price (js/parseInt price)
                                                  :sold-out sold-out})
-                     (reset! modal false))]
+                     (toggle-modal {:active false :gig initial-values}))]
     (fn
       []
       [:main
        [:div.gigs
         [:button.add-gig
-         {:on-click #(reset! modal true)}
+         {:on-click #(toggle-modal {:active true :gig initial-values})}
          [:div.add__title
           [:i.icon.icon--plus]
           [:p "Add gig"]]]
-        [gig-editor modal values insert-gig]
-        (for [{:keys [id img title price desc]}  (vals @state/gigs)] ;;(map (fn [gig] 
+        [gig-editor {:modal modal 
+                     :values values 
+                     :upsert-gig upsert-gig 
+                     :toggle-modal toggle-modal
+                     :initial-values initial-values}]
+        (for [{:keys [id img title price desc] :as gig}  (vals @state/gigs)] ;;(map (fn [gig] 
           [:div.gig {:key id}
-           [:img.gig__artwork.gig__edit {:src img :alt title}]
+           [:img.gig__artwork.gig__edit {:src img 
+                                         :alt title
+                                         :on-click #(toggle-modal {:active true
+                                                                   :gig gig})}]
            [:div.gig__body
             [:div.gig__title
              [:div.btn.btn--primary.float--right.tooltip
